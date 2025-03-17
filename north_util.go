56da889eb2c8ef07e6cdb0e66951ca9ebaf20e97
lazyer <624654852@qq.com>
2025-03-17 14:49:41 +0800

    update

--- north_util.go
+++ north_util.go
@@ -99,7 +99,7 @@ func Query[T any](sql string, params []any, ds *DataSource) ([]*T, error) {
 		return nil, err
 	}
 	defer rows.Close()
-	return RowsToStructPtrs[T](rows)
+	return RowsToStruct[T](rows)
 }
 
 // 预处理查询func RowsToStruct[T any](rows *sql.Rows) ([]T, error) {
@@ -191,57 +191,6 @@ func PrepareDelete(sql string, params []any, ds *DataSource) (int64, error) {
 
 // 把查询结果映射为实体
 func RowsToStruct[T any](rows *sql.Rows) ([]*T, error) {
-	// 创建一个切片的类型，元素为指针类型
-	sliceType := reflect.SliceOf(reflect.PtrTo(reflect.TypeOf(new(T)).Elem()))
-	// 创建一个具有给定长度和容量的切片
-	sliceValue := reflect.MakeSlice(sliceType, 0, 0)
-
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-
-	// 构建字段到列索引的映射
-	fieldToColIndex := make(map[string]int)
-
-	structType := reflect.TypeOf(new(T)).Elem()
-	if structType.Kind() == reflect.Ptr {
-		return nil, errors.New("struct must be a non-pointer type")
-	}
-	for i, columnName := range columns {
-		for j := 0; j < structType.NumField(); j++ {
-			field := structType.Field(j)
-			if tagValue, ok := getFieldTagValue(field, "orm"); ok && tagValue == columnName {
-				fieldToColIndex[field.Name] = i
-				break
-			}
-		}
-	}
-
-	for rows.Next() {
-		var elem T
-		elemValue := reflect.ValueOf(&elem).Elem()
-		scanArgs := make([]any, len(columns))
-		for columnName, colIndex := range fieldToColIndex {
-			field := elemValue.FieldByName(columnName)
-			if !field.IsValid() {
-				return nil, fmt.Errorf("field %s not found in type %T", columnName, elem)
-			}
-			scanArgs[colIndex] = field.Addr().Interface()
-		}
-
-		if err := rows.Scan(scanArgs...); err != nil {
-			return nil, err
-		}
-		// 将 elem 的地址（即指针）添加到切片中
-		sliceValue = reflect.Append(sliceValue, reflect.ValueOf(&elem))
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return sliceValue.Interface().([]*T), nil
-}
-func RowsToStructPtrs[T any](rows *sql.Rows) ([]*T, error) {
 	columns, err := rows.Columns()
 	if err != nil {
 		return nil, err
@@ -335,50 +284,6 @@ func forEachField(structType reflect.Type, fn func(reflect.StructField) error) e
 		}
 	}
 	return nil
-}
-
-// getFieldTagValue 获取结构体字段的tag值
-func getFieldTagValue(field reflect.StructField, tagName string) (string, bool) {
-	if tag, ok := field.Tag.Lookup(tagName); ok {
-		parts := strings.Split(tag, ",")
-		return parts[0], true
-	}
-	return "", false
-}
-func RowsToMapSlice(rows *sql.Rows) ([]map[string]any, error) {
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	results := make([]map[string]any, 0)
-	values := make([]any, len(columns))
-	scanArgs := make([]any, len(values))
-	for i := range values {
-		scanArgs[i] = &values[i]
-	}
-
-	for rows.Next() {
-		rowMap := make(map[string]any)
-		err := rows.Scan(scanArgs...)
-		if err != nil {
-			return nil, err
-		}
-
-		for i, col := range columns {
-			var value any = values[i]
-			if b, ok := value.([]byte); ok {
-				value = string(b)
-			}
-			rowMap[col] = value
-		}
-		results = append(results, rowMap)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return results, nil
 }
 func prepareConvert(sqlStr, driverName string) string {
 	if driverName == DRIVER_NAME_MYSQL {
